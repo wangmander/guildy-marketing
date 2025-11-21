5e2a0471d87b5ff7e4010a33073a2ad997ca0e15
wangmander <153228408+wangmander@users.noreply.github.com>
2025-11-20 21:17:44 -0800

    Update MarketingSessionProvider.tsx

--- components/MarketingSessionProvider.tsx
+++ components/MarketingSessionProvider.tsx
@@ -1,1 +1,7 @@
+"use client"
 
+import { SessionProvider } from "next-auth/react"
+
+export default function MarketingSessionProvider({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>
+}
